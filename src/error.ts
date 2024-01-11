619155910da02e958543fd4bb130f72a76121600
Yazalde Filimone <yazaldefilimon@gmail.com>
2024-01-12 00:05:10 +0200

    chore: prettier format

--- src/error.ts
+++ src/error.ts
@@ -1,7 +1,7 @@
 import { Error } from './types'
-export const errors: Map<number,Error> = new Map()
+export const errors: Map<number, Error> = new Map()
 export function error(pos: number, message: string) {
-    if (!errors.has(pos)) {
-        errors.set(pos, { pos, message })
-    }
+  if (!errors.has(pos)) {
+    errors.set(pos, { pos, message })
+  }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,27 @@
 import fs = require('fs')
 import { compile } from './compile'
 
-const args = process.argv.slice(2);
-const title = (str: string) => console.log('\x1b[1m%s\x1b[0m', str);
+const args = process.argv.slice(2)
+const title = (str: string) => console.log('\x1b[1m%s\x1b[0m', str)
 
 if (!args.length) {
-    console.error("Expected a path to a TS file as the argument")
-    process.exit(1)
+  console.error('Expected a path to a TS file as the argument')
+  process.exit(1)
 }
 
 title(`Looking at: ${args[0]}\n`)
 const ts = fs.readFileSync(args[0], 'utf8')
 const [_tree, errors, js] = compile(ts)
 
-
-title("> TS input:")
+title('> TS input:')
 console.log(ts)
 
 if (errors.length) {
-    title("> Errors:")
-    console.log(errors)
+  title('> Errors:')
+  console.log(errors)
 }
 
-title("> Output:")
+title('> Output:')
 console.log(js)
 
 // Print errors, write js to file
